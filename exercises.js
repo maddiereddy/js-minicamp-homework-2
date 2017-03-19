40cddb3384ca662ff7f42e195d0b2f97de4bade4
maddiereddy <maddiereddy@gmail.com>
2017-03-19 16:23:50 -0700

    passed all tests

--- exercises.js
+++ exercises.js
@@ -3,6 +3,11 @@
 function getBiggest(x, y) {
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
+  if (x > y || x === y)
+    return x;
+  else 
+    return y;
+
 }
 
 function greeting(language) {
@@ -11,15 +16,32 @@ function greeting(language) {
   //language: 'English' -> 'Hello!'
   //language: 'Spanish' -> 'Hola!'
   //if language is undefined return 'Hello!'
+  switch(language) {
+    case 'German':
+        return 'Guten Tag!';
+    case 'Spanish':
+        return 'Hola!';
+    default:
+        return 'Hello!';    
+  }
+
 }
 
 function isTenOrFive(num) {
   //return true if num is 10 or 5
   //otherwise return false
+  if (num === 10 || num === 5)
+    return true;
+  else 
+    return false;
 }
 
 function isInRange(num) {
   //return true if num is less than 50 and greater than 20
+  if (num < 50 && num > 20)
+    return true;
+  else
+    return false;
 }
 
 function isInteger(num) {
@@ -29,6 +51,10 @@ function isInteger(num) {
   //-10 -> true
   //otherwise return false
   //hint: you can solve this using Math.floor
+  if ((num !== 0) && (Math.floor(num) === 0))
+    return false;
+  else 
+    return true;  
 }
 
 function fizzBuzz(num) {
@@ -36,6 +62,16 @@ function fizzBuzz(num) {
   //if num is divisible by 5 return 'buzz'
   //if num is divisible by 3 & 5 return 'fizzbuzz'
   //otherwise return num
+  var test = '';
+  if ((num % 3) === 0)
+    test = 'fizz';
+  if((num % 5) === 0)
+    test += 'buzz';
+  
+  if (test === '')
+    return num;
+  else
+    return test;
 }
 
 function isPrime(num) {
@@ -44,35 +80,55 @@ function isPrime(num) {
   //hint: a prime number is only evenly divisible by itself and 1
   //hint2: you can solve this using a for loop
   //note: 0 and 1 are NOT considered prime numbers
+  if (num === 0 || num === 1) 
+    return false;
+
+  for (var i=2; i<num; i++){
+    if ((num % i) === 0)
+      return false;
+  }
+  return true;
 }
 
 function returnFirst(arr) {
   //return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   //return the last item of the array
+  return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   //return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   //arr is an array of integers  
   //increase each integer by one
   //return the array
+  
+  return arr.map(function(num) {
+    return ++num;
+  });
 }
 
 function addItemToArray(arr, item) {
   //add the item to the end of the array
   //return the array
+  arr.push(item);
+
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   //add the item to the front of the array
   //return the array
   //hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
@@ -80,26 +136,44 @@ function wordsToSentence(words) {
   //return a string that is all of the words concatenated together
   //spaces need to be between each word
   //example: ['Hello', 'world!'] -> 'Hello world!'
+  var test = '';
+
+  for (var i=0; i<words.length; i++){
+    test += words[i] + ' ';
+  }
+  return test.trim();
 }
 
 function contains(arr, item) {
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
+  return arr.includes(item);
 }
 
 function addNumbers(numbers) {
   //numbers is an array of integers.
   //add all of the integers and return the value
+  var sum = 0;
+
+  for (var i=0; i<numbers.length; i++){
+    sum += numbers[i];
+  }
+  return sum;
 }
 
 function averageTestScore(testScores) {
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
+  return addNumbers(testScores) / testScores.length;
 }
 
 function largestNumber(numbers) {
   //numbers is an array of integers
   //return the largest integer
+  numbers.sort(function(a, b) {
+    return a - b;
+  });
+  return numbers[numbers.length - 1];
 }
 
 //Do not modify code below this line.
